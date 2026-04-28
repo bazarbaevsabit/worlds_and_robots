dfcb3e8795cbc9d4b8be055a3b44035587df1d5d
bazarbaevsabit <bazarbaevsabit@gmail.com>
2026-04-28 11:37:21 +0600

    исправил небольшие неточности

--- run.sh
+++ run.sh
@@ -8,13 +8,12 @@ docker run -it --rm \
     -e DISPLAY=$DISPLAY \
     -e XDG_RUNTIME_DIR=$XDG_RUNTIME_DIR \
     -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
-    -v /home/ros2/my_project/worlds_and_robots/src:/home/work \
+    -v /home/user/worlds_and_robots/src:/home/work \
     --device /dev/dri:/dev/dri \
     --name gazebo_test \
     ros2_full:v1 \
     /bin/bash -c "
-            cd /home/work/mobile
-            colcon build
+            cd /home/work/
             exec /bin/bash
         "
 
--- src/mobile/include/mobile/color_utils.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef MOBILE__COLOR_UTILS_HPP_
-#define MOBILE__COLOR_UTILS_HPP_
-
-#include <string>
-
-std::string getColor(float h, float s, float v);
-
-#endif  // MOBILE__COLOR_UTILS_HPP_
